2bfc028ac67f7523bad75d1ff946ddc25d3baa38
Alok Chaudhary <alok.codewing@gmail.com>
2025-05-22 17:16:16 +0545

    ROUTE :: prefix removed

--- routes/web.php
+++ routes/web.php
@@ -3,16 +3,11 @@
 use App\Http\Controllers\HBL\PaymentController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::group(['prefix' => 'hbl', 'as' => 'hbl.'], function () {
-    Route::get('/payment', [PaymentController::class, 'index'])->name('payment.index');
-    Route::post('/payment/checkout', [PaymentController::class, 'checkout'])->name('payment.checkout');
-    Route::post('/payment', [PaymentController::class, 'store'])->name('payment.store');
-    Route::get('/payment/success', [PaymentController::class, 'success'])->name('payment.success');
-    Route::get('/payment/failed', [PaymentController::class, 'failed'])->name('payment.failed');
-    Route::get('/payment/cancel', [PaymentController::class, 'cancel'])->name('payment.cancel');
-    Route::get('/payment/callback', [PaymentController::class, 'callback'])->name('payment.callback');
-});
+Route::get('/', function () { return view('welcome'); });
+Route::get('/payment', [PaymentController::class, 'index'])->name('hbl.payment.index');
+Route::post('/payment/checkout', [PaymentController::class, 'checkout'])->name('hbl.payment.checkout');
+Route::post('/payment', [PaymentController::class, 'store'])->name('hbl.payment.store');
+Route::get('/payment/success', [PaymentController::class, 'success'])->name('hbl.payment.success');
+Route::get('/payment/failed', [PaymentController::class, 'failed'])->name('hbl.payment.failed');
+Route::get('/payment/cancel', [PaymentController::class, 'cancel'])->name('hbl.payment.cancel');
+Route::get('/payment/callback', [PaymentController::class, 'callback'])->name('hbl.payment.callback');
